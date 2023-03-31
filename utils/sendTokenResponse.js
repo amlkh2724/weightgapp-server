8c961b72a5e6ff420262894a5238f935c597600f
amal khanjer <amlkh2724@gmail.com>
2023-03-31 18:21:34 +0300

    fixed token

--- utils/sendTokenResponse.js
+++ utils/sendTokenResponse.js
@@ -1,29 +1,31 @@
-// Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
-  
-    const options = {
-      expires: new Date(
-        // Convert the 30 days in the config t0 30 days in milliseconds
-        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true
-    };
-  
-    // Send secure cookie in production
-    if (process.env.NODE_ENV === 'production') {
-      options.secure = true;
-    }
-  
-    // It's up to the client-side to decide how to handle the token
-    res
-      .status(statusCode)
-      .cookie('token', token, options)
-      .json({
-        success: true,
-        token
-      });
+  const token = user.getSignedJwtToken();
+  const userInfo = {
+    username: user.username,
+    email: user.email,
+    gender: user.gender,
+    height: user.height,
+    weight: user.weight,
+    role: user.role,
+    weightRecords: user.weightRecords,
+    weekTracker: user.weekTracker,
+    id: user._id
   };
-  
-  export default sendTokenResponse;
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token,
+      user: userInfo
+    });
+};
+
+export default sendTokenResponse;
