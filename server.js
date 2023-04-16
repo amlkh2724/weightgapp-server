1e2708d283dbbf9c1e21d8b425e2c9a785e66976
amal khanjer <amlkh2724@gmail.com>
2023-04-16 21:53:42 +0300

    fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/adminPage", admin);
 
 const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
-
+// 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
